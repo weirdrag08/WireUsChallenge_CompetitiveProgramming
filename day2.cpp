fdbfb34dfa5314d73a1c627fb2f24fb58325969d
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-09-19 23:08:04 +0530

    Update day2.cpp

--- day2.cpp
+++ day2.cpp
@@ -1,36 +1,19 @@
 #include<iostream>
 using namespace std;
 
-void print_zig_zag(int* *arr, int row, int col){
-    for(int j = 0; j < col; j++){
-        if(j % 2 == 0){
-            for(int i  = 0; i < row; i++){
-                cout << arr[i][j] << " ";
-            }
+void camel_case_print(string str){
+    for(int i = 0; i < str.size(); i++){
+        if((str[i] >= 'A' && str[i] <= 'Z') && i != 0){
+            cout << endl;
         }
-        else{
-            for(int i = row - 1; i >= 0; i--){
-                cout << arr[i][j] << " ";
-            }
-        }
-        cout << endl;
+        cout << str[i];
     }
 }
 
 int main(){
-    int row, col;
-    cout << "Enter the number of rows: " << endl;
-    cin >> row;
-    cout << "Enter the number of col: " << endl;
-    cin >> col;
-    int** arr = new int*[row];
-    cout << "Enter " << (row * col) << " elements in the array: " << endl;
-    for(int i = 0; i < row; i++){
-        arr[i] = new int[col];
-        for(int j = 0; j < col; j++){
-            cin >> arr[i][j];
-        }
-    }
-    cout << "The zig-zag pattern for the given array: " << endl;
-    print_zig_zag(arr, row, col);
+    string str;
+    cout << "Enter a string: " << endl;
+    cin >> str;
+    camel_case_print(str); 
+    cout << endl;
 }
