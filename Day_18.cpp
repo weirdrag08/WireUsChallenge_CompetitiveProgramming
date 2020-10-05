2d563be0d25d032498a59665c3a37e7fc3d4684b
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-05 21:51:10 +0530

    wireuschallenge day18

--- Day_18.cpp
+++ Day_18.cpp
@@ -3,10 +3,8 @@
 using namespace std;
 int main(){
     int size;
-    cout << "Enter the size of the array: " << endl;
     cin >> size;
     vector<int> input(size), v;
-    cout << "Enter " << size << " elements in the array: " << endl;
     for(int i = 0; i < size; i++){
         cin >> input[i];
         v.push_back(input[i]);
