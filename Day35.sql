8f25ddd7dfeb4468840bd59c99b738ddae7547b8
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-23 20:43:56 +0530

    Day35

---
+++ Day35.sql
@@ -0,0 +1,5 @@
+-- Ques 1:) What are the advantages of DBMS?
+-- Ans 1:) More than 1 is correct.
+
+-- Ques 2:) Which of the following is not a data model available for dbms system?
+-- Ans 2:) network model.
